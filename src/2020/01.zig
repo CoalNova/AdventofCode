7e33bfa3a551e787142879fb727dc52da7153de8
coalnova <coalnova@protonmail.com>
2023-11-30 13:12:05 -0500

    2022 Day's 1 and 2

--- src/2020/01.zig
+++ src/2020/01.zig
@@ -1,14 +1,40 @@
+const std = @import("std");
 const utils = @import("../utils.zig");
-const year = 0;
-const day = 0;
+const year = 2020;
+const day = 1;
 const T = i32;
 pub var aoc = utils.Day(T, dayOne, dayTwo, year, day){};
 
 fn dayOne(input: []const u8) T {
-    _ = input;
+    var splits = std.mem.splitScalar(u8, input, '\n');
+    block: while (splits.next()) |split| {
+        const a = std.fmt.parseInt(i32, split, 10) catch continue :block;
+        var copies = splits;
+        while (copies.next()) |copy| {
+            const b = std.fmt.parseInt(i32, copy, 10) catch continue :block;
+            if (a + b == 2020)
+                return a * b;
+        }
+    }
     return 0;
 }
 fn dayTwo(input: []const u8) T {
-    _ = input;
+    var firsts = std.mem.splitScalar(u8, input, '\n');
+    var nums = std.ArrayList(i32).init(utils.gpa);
+    defer nums.deinit();
+    block: while (firsts.next()) |first| {
+        const a = std.fmt.parseInt(i32, first, 10) catch continue :block;
+        nums.append(a) catch continue :block;
+    }
+    // Standard iterator copying stopped working (don't know why)
+    for (nums.items, 0..) |a, i| {
+        for (nums.items[i..], 0..) |b, j| {
+            for (nums.items[j..]) |c| {
+                if (a + b + c == 2020)
+                    return a * b * c;
+            }
+        }
+    }
+
     return 0;
 }
--- src/2020/02.zig
+++ src/2020/02.zig
@@ -1,14 +1,78 @@
+const std = @import("std");
 const utils = @import("../utils.zig");
-const year = 0;
-const day = 0;
+const year = 2020;
+const day = 2;
 const T = i32;
 pub var aoc = utils.Day(T, dayOne, dayTwo, year, day){};
 
 fn dayOne(input: []const u8) T {
-    _ = input;
-    return 0;
+    var sum: i32 = 0;
+    var splits = std.mem.splitScalar(u8, input, '\n');
+    var buff: [32]u8 = undefined;
+    block: while (splits.next()) |split| {
+        for (split, 0..) |c, i| buff[i] = c;
+        buff[split.len] = 0;
+        var nizer = std.zig.Tokenizer.init(buff[0..split.len :0]);
+        var token = nizer.next();
+        if (token.tag != .number_literal) continue :block;
+        const l_bounds = std.fmt.parseInt(
+            i32,
+            split[token.loc.start..token.loc.end],
+            10,
+        ) catch continue :block;
+        token = nizer.next();
+        token = nizer.next();
+        const u_bounds = std.fmt.parseInt(
+            i32,
+            split[token.loc.start..token.loc.end],
+            10,
+        ) catch continue :block;
+        token = nizer.next();
+        const char = split[token.loc.start];
+        token = nizer.next();
+        token = nizer.next();
+        var quant: u8 = 0;
+        for (split[token.loc.start..token.loc.end]) |c| {
+            if (c == char)
+                quant += 1;
+        }
+        if (quant >= l_bounds and quant <= u_bounds)
+            sum += 1;
+    }
+
+    return sum;
 }
 fn dayTwo(input: []const u8) T {
-    _ = input;
-    return 0;
+    var sum: i32 = 0;
+    var splits = std.mem.splitScalar(u8, input, '\n');
+    var buff: [32]u8 = undefined;
+    block: while (splits.next()) |split| {
+        for (split, 0..) |c, i| buff[i] = c;
+        buff[split.len] = 0;
+        var nizer = std.zig.Tokenizer.init(buff[0..split.len :0]);
+        var token = nizer.next();
+        if (token.tag != .number_literal) continue :block;
+        const l_bounds = std.fmt.parseInt(
+            i32,
+            split[token.loc.start..token.loc.end],
+            10,
+        ) catch continue :block;
+        token = nizer.next();
+        token = nizer.next();
+        const u_bounds = std.fmt.parseInt(
+            i32,
+            split[token.loc.start..token.loc.end],
+            10,
+        ) catch continue :block;
+        token = nizer.next();
+        const char = split[token.loc.start];
+        token = nizer.next();
+        token = nizer.next();
+        const char_a = split[token.loc.start + @as(usize, @intCast(l_bounds)) - 1];
+        const char_b = split[token.loc.start + @as(usize, @intCast(u_bounds)) - 1];
+        if ((char_a == char or char_b == char) and char_a != char_b)
+            sum += 1;
+    }
+
+    return sum;
 }
